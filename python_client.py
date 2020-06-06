7054d2b6827b947b2f3b59a7bdbc13a3c687a5bb
alobach1 <01135748@pw.edu.pl>
2020-06-06 23:27:04 +0200

    comments

--- python_client.py
+++ python_client.py
@@ -3,14 +3,8 @@ import sys
 import socket
 
 
-#remove t from received tcp pakiet to catch id client 
-
 n = network.Network(int(sys.argv[1]))
 id = n.connect()
-# dodac szyfrowanie id clienta
-# n.key = szyfrowanie(id)
-# wewnatrz watku tcp wyslac key 
-# klucz publiczny ?
 n.t1.start()
 n.udp_checking(id)
 n.t2.start()
